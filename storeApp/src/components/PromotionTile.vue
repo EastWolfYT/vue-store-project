1aa740c126133e9efee6c1624929905596a2360d
Michał <michalpok2007@gmail.com>
2026-05-14 10:31:01 +0200

    Fix promotion image paths

--- storeApp/src/components/PromotionTile.vue
+++ storeApp/src/components/PromotionTile.vue
@@ -1,63 +1,56 @@
 <template>
     <div class="promotionTile" :style="contStyle" @mousemove="mousemovePromotion" @mouseleave="mouseleavePromotion">
-        <h1 class="promotionHeader"> {{ this.promotion.header }} </h1>
-        <h3 class="promotionDescription"> {{ this.promotion.description }} </h3>
+        <h1 class="promotionHeader"> {{ promotion.header }} </h1>
+        <h3 class="promotionDescription"> {{ promotion.description }} </h3>
     </div>
 </template>
 
 <script>
 export default {
     name: "PromotionTile",
-    props: { promotion: Object }, // komponent nie pobiera danych sam ze store tylko dostaje jedną promocję z rodzica przez props
+    props: { promotion: Object },
 
     data() {
         return {
-            rotateX: 0, // reagowanie ruchu góra-dół
-            rotateY: 0, // reagowanie ruchu lewo-prawo
-            scaleValue: 1 // lekkie powiększenie podczas hovera
+            rotateX: 0,
+            rotateY: 0,
+            scaleValue: 1
         }
     },
 
     computed: {
-        // to computed buduje styl tła
         contStyle() {
-            const { image } = this.promotion // wyciągamy nazwę obrazka z obiektu promotion
-            let imageUrl;
-
-            try {
-                imageUrl = `/src/assets/${image}` // ścieżka do obrazka w assetsach
-                console.log("image", image);
-                console.log("imageUrl", imageUrl);
-            } catch (e) {
-                console.log(e)
-            }
+            const imageUrl = this.getImageUrl(this.promotion.image)
 
             return {
-                background: `url(${imageUrl})`, // zwracanko stylu tła
-                backgroundSize: "cover", // obrazek wypełnia cały kafelek
-                backgroundPosition: "center", // srodek obrazka w tle
-                transform: `perspective(800px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) scale(${this.scaleValue})`, // obrot i powiekszanie
-                transition: "transform 0.12s linear", // plynniejsza animacja piekna
+                background: `url(${imageUrl})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                transform: `perspective(800px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) scale(${this.scaleValue})`,
+                transition: "transform 0.12s linear",
             }
         }
     },
 
     methods: {
-        mousemovePromotion(e) {
-            const rect = e.currentTarget.getBoundingClientRect() // pobieranko pozycji i rozmiaru kafelka
-
-            const x = e.clientX - rect.left // pozycja myszki w poziomie wewnatrz kafelka
-            const y = e.clientY - rect.top // pozycja myszki w pionie wewnatrz kafelka
-
-            const centerX = rect.width / 2 // srodek kafelka w poziomie
-            const centerY = rect.height / 2 // a tu srodek kafelka w pionie
-
-            // im dalej mysz od srodka tym wiekszy obrot kafelka
-            this.rotateY = ((x - centerX) / centerX) * 8 
-            this.rotateX= ((centerY - y) / centerY) * 8
-            this.scaleValue = 1.05 // lekkie powiekszenie jak najade
+        getImageUrl(image) {
+            return new URL(`../assets/${image}`, import.meta.url).href
         },
-        // wyzerowanko po wysciu myszki
+
+        mousemovePromotion(e) {
+            const rect = e.currentTarget.getBoundingClientRect()
+
+            const x = e.clientX - rect.left
+            const y = e.clientY - rect.top
+
+            const centerX = rect.width / 2
+            const centerY = rect.height / 2
+
+            this.rotateY = ((x - centerX) / centerX) * 8 
+            this.rotateX = ((centerY - y) / centerY) * 8
+            this.scaleValue = 1.05
+        },
+
         mouseleavePromotion() {
             this.rotateX = 0 
             this.rotateY = 0
